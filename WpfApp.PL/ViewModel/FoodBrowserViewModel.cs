de2e0991860c562ef6eb18a6782aff2679cf35df
Chasky <chaskyhoffmann@gmail.com>
2018-10-23 19:01:19 +0300

    live charts

--- WpfApp.PL/ViewModel/FoodBrowserViewModel.cs
+++ WpfApp.PL/ViewModel/FoodBrowserViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using LiveCharts;
+using LiveCharts.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,12 +23,25 @@ namespace WpfApp.PL.ViewModel
                     (x) => ViewNutritions(x)
                 );
 
+            Labels = new String[] {
+                        "Calories",
+                        "Cholestrol",
+                        "Sugars",
+                        "Saturated Fat",
+                        "Total Fat",
+                        "Total Carbohydrate",
+                        "Sodium",
+                        "Protien",
+                        "Potassium"
+                    };
+            Formatter = value => value.ToString("N");
+
             if (IsInDesignMode)
             {
                 FoodNutritions = new FoodNutritions()
                 {
-                    FoodName="Food Name", ServingUnit="Cups", ServingQuantity=2,
-                    ServingWeight="10"
+                    FoodName="Food Name", ServingUnit="Cups", ServingQuantity=2,ServingWeight="10",
+                    TotalFat=2,SaturatedFat=5,Protien=9,Calories=7,Cholestrol=7
                 };
 
                 Foods = new List<BasicFood>()
@@ -35,6 +50,27 @@ namespace WpfApp.PL.ViewModel
                     new BasicFood() { Name="Pear" }
                 };
             }
+
+           // NutritionsSeriesCollection = new SeriesCollection();
+
+            FoodNutritions = new FoodNutritions()
+            {
+                FoodName = "Food Name",
+                ServingUnit = "Cups",
+                ServingQuantity = 2,
+                ServingWeight = "10",
+                TotalFat = 2,
+                SaturatedFat = 5,
+                Protien = 9,
+                Calories = 7,
+                Cholestrol = 7
+            };
+
+            Foods = new List<BasicFood>()
+                {
+                    new BasicFood()  { Name="Apple" },
+                    new BasicFood() { Name="Pear" }
+                };
         }
 
         public ICommand SearchCommand { get; set; }
@@ -111,9 +147,93 @@ namespace WpfApp.PL.ViewModel
                     return;
                 }
                 _FoodNutritions = value;
+
+                if (_FoodNutritions != null)
+                {
+                    NutritionsSeriesCollection = new SeriesCollection()
+                    {
+                        new ColumnSeries()
+                        {
+                            Values = new ChartValues<double>() {
+                                _FoodNutritions.Calories,
+                                _FoodNutritions.Cholestrol,
+                                _FoodNutritions.Sugars,
+                                _FoodNutritions.SaturatedFat,
+                                _FoodNutritions.TotalFat,
+                                _FoodNutritions.TotalCarbohydrate,
+                                _FoodNutritions.Sodium,
+                                _FoodNutritions.Protien,
+                                _FoodNutritions.Potassium
+                            }
+                        }
+                    };
+                }
+
                 RaisePropertyChanged("FoodNutritions");
             }
         }
+
+        /// <summary>
+        /// The <see cref="NutritionsSeriesCollection" /> property.
+        /// </summary>
+        private SeriesCollection _NutritionsSeriesCollection = null;
+        public SeriesCollection NutritionsSeriesCollection
+        {
+            get
+            {
+                return _NutritionsSeriesCollection;
+            }
+            set
+            {
+                if (_NutritionsSeriesCollection == value)
+                {
+                    return;
+                }
+                _NutritionsSeriesCollection = value;
+                RaisePropertyChanged("NutritionsSeriesCollection");
+            }
+        }
         
+        /// <summary>
+        /// The <see cref="Labels" /> property.
+        /// </summary>
+        private String[] _Labels = null;
+        public String[] Labels
+        {
+            get
+            {
+                return _Labels;
+            }
+            set
+            {
+                if (_Labels == value)
+                {
+                    return;
+                }
+                _Labels = value;
+                RaisePropertyChanged("Labels");
+            }
+        }
+
+        /// <summary>
+        /// The <see cref="Formatter" /> property.
+        /// </summary>
+        private Func<double, string> _Formatter = null;
+        public Func<double, string> Formatter
+        {
+            get
+            {
+                return _Formatter;
+            }
+            set
+            {
+                if (_Formatter == value)
+                {
+                    return;
+                }
+                _Formatter = value;
+                RaisePropertyChanged("Formatter");
+            }
+        }
     }
 }
